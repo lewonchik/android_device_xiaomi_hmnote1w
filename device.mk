eaf6dc7803e56937990ee976ad0c42c66c79f119
Rohan Taneja <rohantaneja63@gmail.com>
2016-06-19 00:41:42 +0530

    hmnote1w: initialize YGPS and fix vendor path

--- device.mk
+++ device.mk
@@ -1,6 +1,25 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-$(call inherit-product-if-exists, vendor/xiaomi/hmnote1w/lcsh92_wet_jb9-vendor.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product-if-exists, vendor/xiaomi/hmnote1w/hmnote1w-vendor.mk)
 
 LOCAL_PATH := device/xiaomi/hmnote1w
 
@@ -150,6 +169,10 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
 	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
+# GPS
+PRODUCT_PACKAGES += \
+    YGPS
+    
 # USB
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp,adb
